843d52c6762f3ec2ce0c01e42db7664124e65829
Johng117 <johngorman117@gmail.com>
2025-07-31 09:46:10 +0100

    adding error states, tidying up styling

--- index.js
+++ index.js
@@ -4,13 +4,47 @@ const tipBtns = document.getElementsByClassName("tip");
 const tipForm = document.getElementById("tip-form");
 const tipsArray = [...tipBtns];
 const styles = window.getComputedStyle(customBtn);
+const billInputElement = document.getElementById("bill-amount");
+const peopleInputElement = document.getElementById("people-no");
+const billErrorElement = document.getElementById("bill-amount-error");
+const peopleErrorElement = document.getElementById("people-no-error");
+
+// error functions
+function billValueError(value) {
+  if (value <= 0) {
+    billErrorElement.style.display = "inline-block";
+    billInputElement.style.border = "2px solid hsl(0,100%,50%)";
+    return false;
+  }
+}
+
+function noOfPeopleError(value) {
+  if (value <= 0) {
+    peopleErrorElement.style.display = "inline-block";
+    peopleInputElement.style.border = "2px solid hsl(0,100%,50%)";
+    return false;
+  }
+}
+
+// functions to reset input states
+function resetBillInput() {
+  resetSelectTip();
+  billErrorElement.style.display = "none";
+  billInputElement.style.border = "2px solid hsl(172, 67%, 45%)";
+}
+
+function resetPeopleNumber() {
+  resetSelectTip();
+  peopleErrorElement.style.display = "none";
+  peopleInputElement.style.border = "2px solid hsl(172, 67%, 45%)";
+}
 
 // function to remove the custom button and show the custom input field
 function customBtnOn() {
   if (styles.display === "block") {
     customBtn.style.display = "none";
     customInput.style.display = "block";
-    customInput.style.border ="2px solid hsl(172, 67%, 45%)"
+    customInput.style.border = "2px solid hsl(172, 67%, 45%)";
     customInput.focus();
     resetSelectTip();
   }
@@ -26,8 +60,8 @@ function customBtnOff() {
 }
 
 // handle custom input
-function customSubmit() {
-  
+function customSubmit(e) {
+  handleFormInputs(e.target.value);
 }
 
 // function to reset all tip buttons
@@ -48,13 +82,21 @@ function toggleTip(e) {
   currentBtn.style.backgroundColor = "hsl(172, 67%, 45%)";
 }
 
-
+// calculation function
+function calcTip(dataObj) {
+  
+}
 
 // function to handle form data
 function handleFormInputs(percentage) {
   const data = Object.fromEntries(new FormData(tipForm));
   data["tip"] = percentage;
-  console.log("data", data);
+  const validBill = billValueError(Number(data["bill-amount"]));
+  const validPeople = noOfPeopleError(Number(data["people-no"]));
+
+  if (validBill && validPeople) {
+    calcTip(data);
+  }
 }
 
 // event listeners
@@ -64,3 +106,5 @@ tipsArray.forEach((tip) => {
 
 customBtn.addEventListener("click", customBtnOn);
 customInput.addEventListener("click", customSubmit);
+billInputElement.addEventListener("change", resetBillInput);
+peopleInputElement.addEventListener("change", resetPeopleNumber);
